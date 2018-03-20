3d20e89a7828c387b85fbe4ca3f50838c5e5d05d
Malik Olivier Boussejra <malik@boussejra.com>
2018-03-20 21:04:48 +0900

    Move files in test

--- tests/algo.rs
+++ tests/algo.rs
@@ -1,30 +1,7 @@
-extern crate aflak_backend;
-use aflak_backend::*;
+mod support;
+use support::*;
 
 use std::borrow::Cow;
-
-/// Define specific types used in the examples
-#[derive(Clone, PartialEq, Debug)]
-pub enum AlgoType {
-    Integer,
-    Image2d,
-}
-
-#[derive(Clone, PartialEq, Debug)]
-pub enum AlgoContent {
-    Integer(u64),
-    Image2d(Vec<Vec<f64>>),
-}
-
-impl TypeContent for AlgoContent {
-    type Type = AlgoType;
-    fn get_type(&self) -> Self::Type {
-        match self {
-            &AlgoContent::Integer(_) => AlgoType::Integer,
-            &AlgoContent::Image2d(_) => AlgoType::Image2d,
-        }
-    }
-}
 
 fn plus1(input: Vec<Cow<AlgoContent>>) -> Vec<AlgoContent> {
     if let AlgoContent::Integer(i) = *input[0] {
