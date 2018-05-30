44a2719421d52df73f4c7bdc4a7b1bb27348412e
Malik Olivier Boussejra <malik@boussejra.com>
2018-05-31 00:21:00 +0900

    [cake] Add function to iterate over node's default values

--- src/aflak_cake/src/dst/node.rs
+++ src/aflak_cake/src/dst/node.rs
@@ -23,6 +23,8 @@ pub enum Node<'a, T: 'a + Clone, E: 'a> {
     Output(Option<&'a Output>),
 }
 
+const OUTPUT_NODE_SLOT: TypeId = "Out";
+
 impl<'a, T: Clone, E> Node<'a, T, E> {
     /// Get node's name.
     pub fn name(&'a self, id: &NodeId) -> Cow<'static, str> {
@@ -40,12 +42,20 @@ impl<'a, T: Clone, E> Node<'a, T, E> {
 
     /// Iterate over each type of the inputs
     pub fn inputs_iter(&'a self) -> impl Iterator<Item = &'a TypeId> {
-        const OUTPUT_NODE_SLOT: TypeId = "Out";
         match self {
             &Node::Transform(t) => t.input.as_slice(),
             &Node::Output(_) => &[(OUTPUT_NODE_SLOT, None)],
         }.iter()
             .map(|(id, _)| id)
+    }
+
+    /// Iterate over each default value
+    pub fn inputs_default_iter(&'a self) -> impl Iterator<Item = Option<&'a T>> {
+        match self {
+            &Node::Transform(t) => t.input.as_slice(),
+            &Node::Output(_) => &[(OUTPUT_NODE_SLOT, None)],
+        }.iter()
+            .map(|(_, default)| default.as_ref())
     }
 
     /// Return number of inputs
