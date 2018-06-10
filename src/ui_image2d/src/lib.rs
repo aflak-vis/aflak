22201df37f1a6a78ebd178d54edc71092788afb8
Malik Olivier Boussejra <malik@boussejra.com>
2018-06-10 09:56:51 +0900

    [ui_image2d] Update rustfmt to 0.8.2-nightly (08da30d 2018-06-06)
    Update formatting to latest format.

--- src/ui_image2d/src/lib.rs
+++ src/ui_image2d/src/lib.rs
@@ -1,5 +1,6 @@
 extern crate aflak_plot;
 
 pub use aflak_plot::{
-    imshow::{State, UiImage2d}, Error, InteractionId, Value, ValueIter,
+    imshow::{State, UiImage2d},
+    Error, InteractionId, Value, ValueIter,
 };
