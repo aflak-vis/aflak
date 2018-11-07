11a13f0342839442e642d17c6d88aa930f07d77f
Malik Olivier Boussejra <malik@boussejra.com>
2018-11-08 08:29:27 +0900

    [aflak] Define MyConstantEditor in separate file

---
+++ src/src/constant_editor.rs
@@ -0,0 +1,65 @@
+use imgui_file_explorer::{UiFileExplorer, TOP_FOLDER};
+use node_editor::ConstantEditor;
+use primitives;
+
+use imgui::{ImString, Ui};
+
+#[derive(Default)]
+pub struct MyConstantEditor;
+
+impl ConstantEditor<primitives::IOValue> for MyConstantEditor {
+    fn editor(&self, ui: &Ui, constant: &mut primitives::IOValue) -> bool {
+        use primitives::IOValue;
+
+        ui.push_id(constant as *const primitives::IOValue as i32);
+        let changed = match constant {
+            &mut IOValue::Str(ref mut string) => {
+                let mut out = ImString::with_capacity(1024);
+                out.push_str(string);
+                let changed = ui.input_text(im_str!("String value"), &mut out).build();
+                *string = out.to_str().to_owned();
+                changed
+            }
+            &mut IOValue::Integer(ref mut int) => {
+                let mut out = *int as i32;
+                let changed = ui.input_int(im_str!("Int value"), &mut out).build();
+                *int = out as i64;
+                changed
+            }
+            &mut IOValue::Float(ref mut float) => {
+                ui.input_float(im_str!("Float value"), float).build()
+            }
+            &mut IOValue::Float2(ref mut floats) => {
+                ui.input_float2(im_str!("2 floats value"), floats).build()
+            }
+            &mut IOValue::Float3(ref mut floats) => {
+                ui.input_float3(im_str!("3 floats value"), floats).build()
+            }
+            &mut IOValue::Path(ref mut file) => {
+                ui.text(file.to_str().unwrap_or("Unrepresentable path"));
+                let size = ui.get_item_rect_size();
+
+                let mut ret = Ok(None);
+                ui.child_frame(im_str!("edit"), (size.0.max(200.0), 150.0))
+                    .scrollbar_horizontal(true)
+                    .build(|| {
+                        ret = ui.file_explorer(TOP_FOLDER, &["fits"]);
+                    });
+                if let Ok(Some(new_file)) = ret {
+                    if *file != new_file {
+                        *file = new_file;
+                        true
+                    } else {
+                        false
+                    }
+                } else {
+                    false
+                }
+            }
+            _ => false,
+        };
+        ui.pop_id();
+
+        changed
+    }
+}
--- src/src/main.rs
+++ src/src/main.rs
@@ -12,6 +12,7 @@ extern crate aflak_primitives as primitives;
 extern crate imgui_file_explorer;
 extern crate node_editor;
 
+mod constant_editor;
 mod layout;
 mod save_output;
 
@@ -19,7 +20,7 @@ use std::collections::HashMap;
 use std::env;
 use std::io::Cursor;
 
-use node_editor::{ComputationState, ConstantEditor, NodeEditor};
+use node_editor::{ComputationState, NodeEditor};
 
 use aflak_plot::{
     imshow::{self, UiImage2d},
@@ -27,71 +28,11 @@ use aflak_plot::{
     AxisTransform, InteractionId, ValueIter,
 };
 use imgui::{ImGuiCond, ImStr, ImString, ImTexture, Textures, Ui};
-use imgui_file_explorer::UiFileExplorer;
 
+use constant_editor::MyConstantEditor;
 use layout::LayoutEngine;
 
 const CLEAR_COLOR: [f32; 4] = [0.05, 0.05, 0.05, 1.0];
-
-#[derive(Default)]
-struct MyConstantEditor;
-
-impl ConstantEditor<primitives::IOValue> for MyConstantEditor {
-    fn editor(&self, ui: &Ui, constant: &mut primitives::IOValue) -> bool {
-        use primitives::IOValue;
-
-        ui.push_id(constant as *const primitives::IOValue as i32);
-        let changed = match constant {
-            &mut IOValue::Str(ref mut string) => {
-                let mut out = ImString::with_capacity(1024);
-                out.push_str(string);
-                let changed = ui.input_text(im_str!("String value"), &mut out).build();
-                *string = out.to_str().to_owned();
-                changed
-            }
-            &mut IOValue::Integer(ref mut int) => {
-                let mut out = *int as i32;
-                let changed = ui.input_int(im_str!("Int value"), &mut out).build();
-                *int = out as i64;
-                changed
-            }
-            &mut IOValue::Float(ref mut float) => {
-                ui.input_float(im_str!("Float value"), float).build()
-            }
-            &mut IOValue::Float2(ref mut floats) => {
-                ui.input_float2(im_str!("2 floats value"), floats).build()
-            }
-            &mut IOValue::Float3(ref mut floats) => {
-                ui.input_float3(im_str!("3 floats value"), floats).build()
-            }
-            &mut IOValue::Path(ref mut file) => {
-                ui.text(file.to_str().unwrap_or("Unrepresentable path"));
-                let size = ui.get_item_rect_size();
-
-                let mut ret = Ok(None);
-                ui.child_frame(im_str!("edit"), (size.0.max(200.0), 150.0))
-                    .scrollbar_horizontal(true)
-                    .build(|| {
-                        ret = ui.file_explorer(imgui_file_explorer::TOP_FOLDER, &["fits"]);
-                    });
-                if let Ok(Some(new_file)) = ret {
-                    if *file != new_file {
-                        *file = new_file;
-                        true
-                    } else {
-                        false
-                    }
-                } else {
-                    false
-                }
-            }
-            _ => false,
-        };
-        ui.pop_id();
-
-        changed
-    }
-}
 
 fn main() {
     env::set_var("WINIT_HIDPI_FACTOR", "1");
