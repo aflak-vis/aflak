cc62ed5353ad4559c91466b0e7982b06fd769530
Malik Olivier Boussejra <malik@boussejra.com>
2018-11-08 10:19:46 +0900

    [aflak] Make OutputWindow type to draw output window

--- src/src/aflak.rs
+++ src/src/aflak.rs
@@ -23,8 +23,16 @@ pub struct Aflak<'t> {
     layout_engine: LayoutEngine,
     image1d_states: HashMap<ImString, plot::State>,
     image2d_states: HashMap<ImString, imshow::State>,
-    editable_values: HashMap<(OutputId, InteractionId), TransformIdx>,
+    editable_values: EditableValues,
 }
+
+type EditableValues = HashMap<(OutputId, InteractionId), TransformIdx>;
+
+struct OutputWindow {
+    output: OutputId,
+    window_name: ImString,
+}
+
 impl<'t> Aflak<'t> {
     pub fn init(editor: AflakNodeEditor<'t>) -> Self {
         Self {
@@ -55,61 +63,72 @@ impl<'t> Aflak<'t> {
     {
         let outputs = self.node_editor.outputs();
         for output in outputs {
-            let window_name = ImString::new(format!("Output #{}", output.id()));
-            let (position, size) = self
-                .layout_engine
-                .default_output_window_position_size(&window_name);
-            let window = ui
-                .window(&window_name)
-                .position(position, ImGuiCond::FirstUseEver)
-                .size(size, ImGuiCond::FirstUseEver);
-            window.build(|| {
-                let compute_state = unsafe { self.node_editor.compute_output(&output) };
-                let compute_state = &*compute_state.lock().unwrap();
-                match compute_state {
-                    &ComputationState::NothingDone => {
-                        ui.text("Initializing...");
-                    }
-                    &ComputationState::RunningFirstTime => {
-                        ui.text("Computing...");
-                    }
-                    _ => {
-                        if let Some(result) = compute_state.result() {
-                            match result {
-                                Err(e) => ui.text_wrapped(&ImString::new(format!("{}", e))),
-                                Ok(result) => self.output_window_computed_content(
-                                    ui,
-                                    result,
-                                    &output,
-                                    &window_name,
-                                    gl_ctx,
-                                    textures,
-                                ),
-                            };
-                        } else {
-                            // As per the present computation state,
-                            // a result should be present. Else it's a bug.
-                            unreachable!();
-                        }
-                    }
-                }
-            });
+            let mut output_window = self.output_window(output);
+            output_window.draw(ui, self, gl_ctx, textures);
         }
     }
 
-    fn output_window_computed_content<F>(
-        &mut self,
+    fn output_window(&mut self, output: OutputId) -> OutputWindow {
+        OutputWindow {
+            output,
+            window_name: ImString::new(format!("Output #{}", output.id())),
+        }
+    }
+}
+
+impl OutputWindow {
+    fn draw<F>(&self, ui: &Ui, aflak: &mut Aflak, gl_ctx: &F, textures: &mut Textures)
+    where
+        F: glium::backend::Facade,
+    {
+        let (position, size) = aflak
+            .layout_engine
+            .default_output_window_position_size(&self.window_name);
+        let window_name = self.window_name.to_owned();
+        let window = ui
+            .window(&window_name)
+            .position(position, ImGuiCond::FirstUseEver)
+            .size(size, ImGuiCond::FirstUseEver);
+        window.build(|| {
+            let compute_state = unsafe { aflak.node_editor.compute_output(&self.output) };
+            let compute_state = &*compute_state.lock().unwrap();
+            match compute_state {
+                &ComputationState::NothingDone => {
+                    ui.text("Initializing...");
+                }
+                &ComputationState::RunningFirstTime => {
+                    ui.text("Computing...");
+                }
+                _ => {
+                    if let Some(result) = compute_state.result() {
+                        match result {
+                            Err(e) => ui.text_wrapped(&ImString::new(format!("{}", e))),
+                            Ok(result) => {
+                                self.computed_content(ui, aflak, result, gl_ctx, textures)
+                            }
+                        };
+                    } else {
+                        // As per the present computation state,
+                        // a result should be present. Else it's a bug.
+                        unreachable!();
+                    }
+                }
+            }
+        });
+    }
+
+    fn computed_content<F>(
+        &self,
         ui: &Ui,
+        aflak: &mut Aflak,
         result: &IOValue,
-        output: &OutputId,
-        window_name: &ImStr,
         gl_ctx: &F,
         textures: &mut Textures,
     ) where
         F: glium::backend::Facade,
     {
         if ui.button(im_str!("Save data"), (0.0, 0.0)) {
-            if let Err(e) = save_output::save(output, result) {
+            if let Err(e) = save_output::save(&self.output, result) {
                 eprintln!("Error on saving output: '{}'", e);
             } else {
                 ui.open_popup(im_str!("FITS export completed!"));
@@ -118,7 +137,7 @@ impl<'t> Aflak<'t> {
         ui.popup_modal(im_str!("FITS export completed!")).build(|| {
             ui.text(format!(
                 "File saved with success to '{}'.",
-                save_output::file_name(output)
+                save_output::file_name(&self.output)
             ));
             if ui.button(im_str!("Close"), (0.0, 0.0)) {
                 ui.close_current_popup();
@@ -144,16 +163,15 @@ impl<'t> Aflak<'t> {
                 ui.text(format!("{:?}", floats));
             }
             &IOValue::Image1d(ref image) => {
-                let state = self
+                let state = aflak
                     .image1d_states
-                    .entry(window_name.to_owned())
+                    .entry(self.window_name.to_owned())
                     .or_insert_with(|| plot::State::default());
 
-                update_state_from_editor(
-                    &output,
+                self.update_state_from_editor(
                     state.stored_values_mut(),
-                    &mut self.editable_values,
-                    &self.node_editor,
+                    &aflak.editable_values,
+                    &aflak.node_editor,
                 );
                 let unit = image.array().unit().repr();
                 let transform = match (image.cunit(), image.wcs()) {
@@ -165,26 +183,24 @@ impl<'t> Aflak<'t> {
                 if let Err(e) = ui.image1d(image.scalar(), &unit, transform, state) {
                     ui.text(format!("Error on drawing plot! {}", e))
                 }
-                update_editor_from_state(
-                    &output,
+                self.update_editor_from_state(
                     state.stored_values(),
-                    &mut self.editable_values,
-                    &mut self.node_editor,
+                    &mut aflak.editable_values,
+                    &mut aflak.node_editor,
                 );
             }
             &IOValue::Image2d(ref image) => {
-                let state = self
+                let state = aflak
                     .image2d_states
-                    .entry(window_name.to_owned())
+                    .entry(self.window_name.to_owned())
                     .or_insert_with(|| imshow::State::default());
 
-                update_state_from_editor(
-                    &output,
+                self.update_state_from_editor(
                     state.stored_values_mut(),
-                    &mut self.editable_values,
-                    &self.node_editor,
+                    &aflak.editable_values,
+                    &aflak.node_editor,
                 );
-                let texture_id = ImTexture::from(hash_imstring(window_name));
+                let texture_id = ImTexture::from(hash_imstring(&self.window_name));
                 let (x_transform, y_transform) = match (image.cunits(), image.wcs()) {
                     (Some(units), Some(wcs)) => (
                         Some(AxisTransform::new(units[0].repr(), move |t| {
@@ -209,11 +225,10 @@ impl<'t> Aflak<'t> {
                 ) {
                     ui.text(format!("Error on drawing image! {}", e));
                 }
-                update_editor_from_state(
-                    &output,
+                self.update_editor_from_state(
                     state.stored_values(),
-                    &mut self.editable_values,
-                    &mut self.node_editor,
+                    &mut aflak.editable_values,
+                    &mut aflak.node_editor,
                 );
             }
             _ => {
@@ -221,63 +236,63 @@ impl<'t> Aflak<'t> {
             }
         }
     }
-}
 
-fn update_state_from_editor(
-    output: &OutputId,
-    interactions: InteractionIterMut,
-    store: &HashMap<(OutputId, InteractionId), TransformIdx>,
-    node_editor: &AflakNodeEditor,
-) {
-    for (id, interaction) in interactions {
-        let value_id = (*output, *id);
-        if store.contains_key(&value_id) {
-            let t_idx = store.get(&value_id).unwrap();
-            if let Some(value) = node_editor.constant_node_value(t_idx) {
-                assert!(
-                    value.len() == 1,
-                    "Only constant nodes with exactly one value are supported",
-                );
-                let value = &value[0];
-                if let Err(e) = match value {
-                    IOValue::Integer(i) => interaction.set_value(*i),
-                    IOValue::Float(f) => interaction.set_value(*f),
-                    IOValue::Float2(f) => interaction.set_value(*f),
-                    IOValue::Float3(f) => interaction.set_value(*f),
-                    value => Err(format!("Cannot convert value '{:?}'", value)),
-                } {
-                    eprintln!("Could not update state from editor: {}", e);
+    fn update_state_from_editor(
+        &self,
+        interactions: InteractionIterMut,
+        editable_values: &EditableValues,
+        node_editor: &AflakNodeEditor,
+    ) {
+        for (id, interaction) in interactions {
+            let value_id = (self.output, *id);
+            if editable_values.contains_key(&value_id) {
+                let t_idx = editable_values.get(&value_id).unwrap();
+                if let Some(value) = node_editor.constant_node_value(t_idx) {
+                    assert!(
+                        value.len() == 1,
+                        "Only constant nodes with exactly one value are supported",
+                    );
+                    let value = &value[0];
+                    if let Err(e) = match value {
+                        IOValue::Integer(i) => interaction.set_value(*i),
+                        IOValue::Float(f) => interaction.set_value(*f),
+                        IOValue::Float2(f) => interaction.set_value(*f),
+                        IOValue::Float3(f) => interaction.set_value(*f),
+                        value => Err(format!("Cannot convert value '{:?}'", value)),
+                    } {
+                        eprintln!("Could not update state from editor: {}", e);
+                    }
+                } else {
+                    eprintln!("No constant node found for transform '{:?}'", t_idx);
                 }
             } else {
-                eprintln!("No constant node found for transform '{:?}'", t_idx);
+                eprintln!("ValueID '{:?}' not found in store", value_id);
             }
-        } else {
-            eprintln!("ValueID '{:?}' not found in store", value_id);
         }
     }
-}
 
-fn update_editor_from_state(
-    output: &OutputId,
-    value_iter: ValueIter,
-    store: &mut HashMap<(OutputId, InteractionId), TransformIdx>,
-    node_editor: &mut AflakNodeEditor,
-) {
-    for (id, value) in value_iter {
-        use aflak_plot::Value;
-        let val = match value {
-            Value::Integer(i) => IOValue::Integer(i),
-            Value::Float(f) => IOValue::Float(f),
-            Value::Float2(f) => IOValue::Float2(f),
-            Value::Float3(f) => IOValue::Float3(f),
-        };
-        let value_id = (*output, *id);
-        if store.contains_key(&value_id) {
-            let t_idx = store.get(&value_id).unwrap();
-            node_editor.update_constant_node(t_idx, vec![val]);
-        } else {
-            let t_idx = node_editor.create_constant_node(val);
-            store.insert(value_id, t_idx);
+    fn update_editor_from_state(
+        &self,
+        value_iter: ValueIter,
+        store: &mut EditableValues,
+        node_editor: &mut AflakNodeEditor,
+    ) {
+        for (id, value) in value_iter {
+            use aflak_plot::Value;
+            let val = match value {
+                Value::Integer(i) => IOValue::Integer(i),
+                Value::Float(f) => IOValue::Float(f),
+                Value::Float2(f) => IOValue::Float2(f),
+                Value::Float3(f) => IOValue::Float3(f),
+            };
+            let value_id = (self.output, *id);
+            if store.contains_key(&value_id) {
+                let t_idx = store.get(&value_id).unwrap();
+                node_editor.update_constant_node(t_idx, vec![val]);
+            } else {
+                let t_idx = node_editor.create_constant_node(val);
+                store.insert(value_id, t_idx);
+            }
         }
     }
 }
