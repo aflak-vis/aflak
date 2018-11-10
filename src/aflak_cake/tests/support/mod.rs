271fc3877aa1f221442cc03319530fc700a90cac
Malik Olivier Boussejra <malik@boussejra.com>
2018-11-10 22:33:21 +0900

    [cake] lifetimes: Got test to compile

--- src/aflak_cake/tests/support/mod.rs
+++ src/aflak_cake/tests/support/mod.rs
@@ -18,28 +18,28 @@ impl fmt::Display for E {
     }
 }
 
-pub fn get_plus1_transform() -> Transformation<AlgoIO, E> {
+pub fn get_plus1_transform() -> Transformation<'static, AlgoIO, E> {
     cake_transform!("Add 1", plus1<AlgoIO, E>(i: Integer = 0) -> Integer {
         vec![Ok(AlgoIO::Integer(i + 1))]
     })
 }
 
-pub fn get_minus1_transform() -> Transformation<AlgoIO, E> {
+pub fn get_minus1_transform() -> Transformation<'static, AlgoIO, E> {
     cake_transform!("Substract 1", minus1<AlgoIO, E>(i: Integer) -> Integer {
         vec![Ok(AlgoIO::Integer(i - 1))]
     })
 }
 
-pub fn get_get1_transform() -> Transformation<AlgoIO, E> {
+pub fn get_get1_transform() -> Transformation<'static, AlgoIO, E> {
     cake_constant!(get1, AlgoIO::Integer(1))
 }
 
-pub fn get_get_image_transform() -> Transformation<AlgoIO, E> {
+pub fn get_get_image_transform() -> Transformation<'static, AlgoIO, E> {
     cake_constant!(image, AlgoIO::Image2d(vec![vec![10.0; 10000]; 10000]))
 }
 
 lazy_static! {
-    pub static ref TRANSFORMATIONS: Vec<Transformation<AlgoIO, E>> = {
+    pub static ref TRANSFORMATIONS: Vec<Transformation<'static, AlgoIO, E>> = {
         vec![
             get_plus1_transform(),
             get_minus1_transform(),
@@ -50,7 +50,7 @@ lazy_static! {
 }
 
 impl NamedAlgorithms<E> for AlgoIO {
-    fn get_transform(s: &str) -> Option<&'static Transformation<AlgoIO, E>> {
+    fn get_transform(s: &str) -> Option<&'static Transformation<'static, AlgoIO, E>> {
         for t in TRANSFORMATIONS.iter() {
             if t.name == s {
                 return Some(t);
