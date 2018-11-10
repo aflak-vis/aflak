b00e89e0430049fd4d6107a07aa1385a36b1197c
Malik Olivier Boussejra <malik@boussejra.com>
2018-11-10 18:15:57 +0900

    [node_editor] id_stack: Fix match_ref_pats

--- src/node_editor/src/id_stack.rs
+++ src/node_editor/src/id_stack.rs
@@ -7,9 +7,9 @@ pub trait GetId {
 
 impl GetId for NodeId {
     fn id(&self) -> i32 {
-        match self {
-            &NodeId::Transform(t_idx) => t_idx.id() as i32,
-            &NodeId::Output(output_id) => -(output_id.id() as i32),
+        match *self {
+            NodeId::Transform(t_idx) => t_idx.id() as i32,
+            NodeId::Output(output_id) => -(output_id.id() as i32),
         }
     }
 }
