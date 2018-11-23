256cab671a3f5103cff1f85bc1da91001a7cfa2a
Malik Olivier Boussejra <malik@boussejra.com>
2018-11-23 13:52:08 +0900

    [cake] future: Add documentation and cleanup

--- src/aflak_cake/src/future.rs
+++ src/aflak_cake/src/future.rs
@@ -4,6 +4,7 @@ use rayon;
 use std::mem;
 use std::sync::{Arc, Mutex};
 
+/// An asynchronous task
 pub struct Task<T, E> {
     state: Arc<Mutex<TaskState<T, E>>>,
 }
@@ -15,23 +16,15 @@ enum TaskState<T, E> {
     Errored(E),
 }
 
-impl<T, E> TaskState<T, E> {
-    fn ready(&self) -> bool {
-        if let TaskState::Ready(_) = *self {
-            true
-        } else {
-            false
-        }
-    }
-}
-
 impl<T, E> Task<T, E> {
+    /// Make a new task that is already finished and successful.
     pub fn resolved(t: T) -> Self {
         Self {
             state: Arc::new(Mutex::new(TaskState::Ready(t))),
         }
     }
 
+    /// Make a new task that is already finished and failed.
     pub fn errored(e: E) -> Self {
         Self {
             state: Arc::new(Mutex::new(TaskState::Errored(e))),
@@ -44,6 +37,7 @@ where
     T: Sync + Send + 'static,
     E: Send + 'static,
 {
+    /// Make a new asynchronous task from closure.
     pub fn new<F>(f: F) -> Self
     where
         F: FnOnce() -> Result<T, E> + Send + 'static,
@@ -70,12 +64,12 @@ impl<T, E> Future for Task<T, E> {
     fn poll(&mut self) -> Poll<Self::Item, Self::Error> {
         match self.state.lock() {
             Ok(mut lock) => {
-                if lock.ready() {
+                if let TaskState::Ready(_) = *lock {
                     let ready_state = mem::replace(&mut *lock, TaskState::Consumed);
                     if let TaskState::Ready(t) = ready_state {
                         Ok(Async::Ready(t))
                     } else {
-                        panic!("Cannot call poll twice!")
+                        unreachable!()
                     }
                 } else if let TaskState::Errored(_) = *lock {
                     let errored_state = mem::replace(&mut *lock, TaskState::Consumed);
