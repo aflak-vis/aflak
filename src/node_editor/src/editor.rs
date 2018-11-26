aflak-vis/aflak

commit a75826a14524e2e9b21a5e676f1c40d148650c63

[cake] Heavily refactor Transform
- Improve naming to reduce confusion:
  - Rename Transformation to Transform
  - Rename TranformId to FnTransformId
- Algorithm::Constant contains a single value, not a vector of value.
Until now, only the first value of the vector was ever used.
- Remove requirement for T to implement Clone.
- Move PlainFunction specific data in Transform to Algorithm type.
- Transform contains an Instant value containing it's latest update
timestamp.

- Update all depending code appropriately.

src/node_editor/src/editor.rs
@@ -7,7 +7,7 @@ use imgui::{
 };
 use serde::{Deserialize, Serialize};
 
-use cake::{self, InputSlot, Transformation, DST};
+use cake::{self, InputSlot, Transform, VariantName, DST};
 
 use compute::{self, ComputeResult};
 use constant_editor::ConstantEditor;
@@ -18,7 +18,7 @@ use vec2::Vec2;
 
 pub struct NodeEditor<'t, T: 't + Clone, E: 't, ED> {
     pub(crate) dst: DST<'t, T, E>,
-    addable_nodes: &'t [&'t Transformation<T, E>],
+    addable_nodes: &'t [&'t Transform<T, E>],
     pub(crate) node_states: NodeStates,
     active_node: Option<cake::NodeId>,
     drag_node: Option<cake::NodeId>,
@@ -68,7 +68,7 @@ where
     T: Clone,
     ED: Default,
 {
-    pub fn new(addable_nodes: &'t [&'t Transformation<T, E>], ed: ED) -> Self {
+    pub fn new(addable_nodes: &'t [&'t Transform<T, E>], ed: ED) -> Self {
         Self {
             addable_nodes,
             constant_editor: ed,
@@ -76,11 +76,7 @@ where
         }
     }
 
-    pub fn from_dst(
-        dst: DST<'t, T, E>,
-        addable_nodes: &'t [&'t Transformation<T, E>],
-        ed: ED,
-    ) -> Self {
+    pub fn from_dst(dst: DST<'t, T, E>, addable_nodes: &'t [&'t Transform<T, E>], ed: ED) -> Self {
         let mut output_results = BTreeMap::new();
         for (output_id, _) in dst.outputs_iter() {
             output_results.insert(*output_id, compute::new_compute_result());
@@ -105,25 +101,24 @@ where
     T: Clone + cake::VariantName,
 {
     pub fn create_constant_node(&mut self, t: T) -> cake::TransformIdx {
-        self.dst
-            .add_owned_transform(Transformation::new_constant(t))
+        self.dst.add_owned_transform(Transform::new_constant(t))
     }
 }
 
 impl<'t, T, E, ED> NodeEditor<'t, T, E, ED>
 where
-    T: Clone + PartialEq,
+    T: Clone + PartialEq + VariantName,
 {
-    pub fn update_constant_node(&mut self, id: cake::TransformIdx, val: Vec<T>) {
+    pub fn update_constant_node(&mut self, id: cake::TransformIdx, val: T) {
         let mut purge = false;
         if let Some(t) = self.dst.get_transform_mut(id) {
-            if let cake::Algorithm::Constant(ref mut constants) = t.algorithm {
-                for (c, val) in constants.iter_mut().zip(val.into_iter()) {
-                    if *c != val {
-                        *c = val;
-                        purge = true;
-                    }
+            if let cake::Algorithm::Constant(ref constant) = t.algorithm() {
+                if *constant == val {
+                    purge = true;
                 }
+            }
+            if purge {
+                t.set_constant(val);
             }
         }
         if purge {
@@ -136,10 +131,10 @@ impl<'t, T, E, ED> NodeEditor<'t, T, E, ED>
 where
     T: Clone,
 {
-    pub fn constant_node_value(&self, id: cake::TransformIdx) -> Option<&[T]> {
+    pub fn constant_node_value(&self, id: cake::TransformIdx) -> Option<&T> {
         self.dst.get_transform(id).and_then(|t| {
-            if let cake::Algorithm::Constant(ref constants) = t.algorithm {
-                Some(constants.as_slice())
+            if let cake::Algorithm::Constant(ref constant) = t.algorithm() {
+                Some(constant)
             } else {
                 None
             }
@@ -252,8 +247,8 @@ where
                                 ui.text_wrapped(&ImString::new(format!(
                                     "#{} {}:\n{}",
                                     node_id.id(),
-                                    t.name,
-                                    t.description
+                                    t.name(),
+                                    t.description()
                                 )));
                             }
                             cake::Node::Output(_) => {
@@ -503,7 +498,9 @@ where
                     // Display connectors
                     const CONNECTOR_BORDER_THICKNESS: f32 = NODE_SLOT_RADIUS * 0.25;
                     const INPUT_SLOT_COLOR: [f32; 4] = [0.59, 0.59, 0.59, 0.59];
-                    for (slot_idx, &slot_name) in node.inputs_iter().enumerate() {
+                    for (slot_idx, slot_name) in
+                        node.input_slot_names_iter().into_iter().enumerate()
+                    {
                         let connector_pos = Vec2::new(node_states.get_state(&idx, |state| {
                             state.get_input_slot_pos(
                                 slot_idx,
@@ -565,7 +562,8 @@ where
                     // Show outputs for transform nodes
                     if let cake::NodeId::Transform(t_idx) = idx {
                         const OUTPUT_SLOT_COLOR: [f32; 4] = [0.59, 0.59, 0.59, 0.59];
-                        for (slot_idx, &slot_name) in node.outputs_iter().enumerate() {
+                        for (slot_idx, type_id) in node.outputs_iter().into_iter().enumerate() {
+                            let slot_name = type_id.name();
                             let connector_pos = node_states.get_state(&idx, |state| {
                                 state.get_output_slot_pos(
                                     slot_idx,
@@ -623,8 +621,12 @@ where
                     if ui.imgui().is_mouse_dragging(ImMouseButton::Left) {
                         let (p1, cp1, cp2, p2) = match *creating_link {
                             LinkExtremity::Output(output) => {
-                                let output_node_count =
-                                    self.dst.get_transform(output.t_idx).unwrap().output.len();
+                                let output_node_count = self
+                                    .dst
+                                    .get_transform(output.t_idx)
+                                    .unwrap()
+                                    .outputs()
+                                    .len();
                                 let output_node_state = self
                                     .node_states
                                     .get(&cake::NodeId::Transform(output.t_idx))
@@ -647,7 +649,7 @@ where
                                             .dst
                                             .get_transform(input.t_idx)
                                             .unwrap()
-                                            .input
+                                            .inputs()
                                             .len();
                                         let input_node_state = self
                                             .node_states
@@ -694,7 +696,7 @@ where
                     let connector_in_pos = match input_slot {
                         cake::InputSlot::Transform(input) => {
                             let input_node_count =
-                                self.dst.get_transform(input.t_idx).unwrap().input.len();
+                                self.dst.get_transform(input.t_idx).unwrap().inputs().len();
                             let input_node_state = self
                                 .node_states
                                 .get(&cake::NodeId::Transform(input.t_idx))
@@ -718,8 +720,12 @@ where
                         }
                     };
                     let p1 = offset + connector_in_pos;
-                    let output_node_count =
-                        self.dst.get_transform(output.t_idx).unwrap().output.len();
+                    let output_node_count = self
+                        .dst
+                        .get_transform(output.t_idx)
+                        .unwrap()
+                        .outputs()
+                        .len();
                     let output_node_state = self
                         .node_states
                         .get(&cake::NodeId::Transform(output.t_idx))
@@ -758,7 +764,7 @@ where
             ui.separator();
             for (i, node) in self.addable_nodes.iter().enumerate() {
                 ui.push_id(i as i32);
-                if ui.menu_item(&ImString::new(node.name)).build() {
+                if ui.menu_item(&ImString::new(node.name())).build() {
                     self.dst.add_transform(node);
                 }
                 ui.pop_id();
@@ -773,7 +779,7 @@ where
             for constant_type in T::editable_variants() {
                 let item_name = ImString::new(format!("Input node: {}", constant_type));
                 if ui.menu_item(&item_name).build() {
-                    let constant = Transformation::new_constant(T::default_for(constant_type));
+                    let constant = Transform::new_constant(T::default_for(constant_type));
                     self.dst.add_owned_transform(constant);
                 }
             }
@@ -813,12 +819,16 @@ where
             let mut purge_list = Vec::new();
             if let cake::NodeId::Transform(t_idx) = *id {
                 if let Some(t) = dst.get_transform_mut(t_idx) {
-                    if let cake::Algorithm::Constant(ref mut constants) = t.algorithm {
-                        for c in constants.iter_mut() {
-                            if constant_editor.editor(ui, c) {
-                                purge_list.push(id);
-                            }
+                    let mut changed = None;
+                    if let cake::Algorithm::Constant(ref constant) = t.algorithm() {
+                        let mut c_copy = constant.clone();
+                        if constant_editor.editor(ui, &mut c_copy) {
+                            purge_list.push(id);
+                            changed = Some(c_copy);
                         }
+                    }
+                    if let Some(c) = changed {
+                        t.set_constant(c);
                     }
                 }
                 if let Some(default_inputs) = dst.get_default_inputs_mut(t_idx) {
@@ -884,7 +894,7 @@ where
 
 impl<'t, T, E, ED> NodeEditor<'t, T, E, ED>
 where
-    T: Clone,
+    T: Clone + VariantName,
 {
     fn delete_selected_nodes(&mut self) {
         let selected_node_ids: Vec<_> = self

src/aflak_cake/src/dst/build.rs
@@ -10,121 +10,20 @@ use variant_name::VariantName;
 use dst::node::{Node, NodeId};
 use dst::{DSTError, Input, InputList, Output, OutputId, TransformIdx, DST};
 use dst::{MetaTransform, TransformAndDefaults};
-use transform::Transformation;
+use transform::Transform;
 
 impl<'t, T: 't, E: 't> DST<'t, T, E>
 where
     T: Clone + VariantName,
 {
-    /// Add a borrowed transform and return its identifier [`TransformIdx`].
-    pub fn add_transform(&mut self, t: &'t Transformation<T, E>) -> TransformIdx {
-        self.add_transform_impl(Bow::Borrowed(t))
-    }
-
-    /// Add an owned transform and return its identifier [`TransformIdx`].
-    pub fn add_owned_transform(&mut self, t: Transformation<T, E>) -> TransformIdx {
-        self.add_transform_impl(Bow::Owned(t))
-    }
-
-    fn add_transform_impl(&mut self, t: Bow<'t, Transformation<T, E>>) -> TransformIdx {
-        let idx = self.new_transform_idx();
-        self.transforms.insert(idx, MetaTransform::new(t));
-        idx
-    }
-}
-
-impl<'t, T: 't, E: 't> DST<'t, T, E>
-where
-    T: Clone,
-{
-    /// Make a new empty [`DST`].
-    pub fn new() -> Self {
-        Self {
-            transforms: HashMap::new(),
-            edges: HashMap::new(),
-            outputs: HashMap::new(),
-            cache: HashMap::new(),
-        }
-    }
-
-    /// Get a transform from its [`TransformIdx`].
-    pub fn get_transform(&self, idx: TransformIdx) -> Option<&Transformation<T, E>> {
-        self.transforms.get(&idx).map(|t| t.transform())
-    }
-
-    /// Get a transform mutably from its [`TransformIdx`].
-    /// Return `None` if the target transform is not owned.
-    pub fn get_transform_mut(&mut self, idx: TransformIdx) -> Option<&mut Transformation<T, E>> {
-        self.transforms
-            .get_mut(&idx)
-            .and_then(|t| t.transform_mut())
-    }
-
-    /// Get a reference to a transform's default inputs from its
-    /// [`TransformIdx`].
-    /// Return [`None`] if the target transform does not exist.
-    pub fn get_default_inputs(&mut self, idx: TransformIdx) -> Option<&[Option<T>]> {
-        self.transforms.get(&idx).map(|t| t.defaults())
-    }
-
-    /// Get a mutable reference to a transform's default inputs from its
-    /// [`TransformIdx`].
-    /// Return [`None`] if the target transform does not exist.
-    pub fn get_default_inputs_mut(&mut self, idx: TransformIdx) -> Option<&mut [Option<T>]> {
-        self.transforms.get_mut(&idx).map(|t| t.defaults_mut())
-    }
-
-    /// Get a node from its [`NodeId`].
-    pub fn get_node(&self, idx: &NodeId) -> Option<Node<T, E>> {
-        match *idx {
-            NodeId::Transform(t_idx) => self.get_transform(t_idx).map(Node::Transform),
-            NodeId::Output(ref output_id) => self
-                .outputs
-                .get(output_id)
-                .map(|some_output| Node::Output(some_output.as_ref())),
-        }
-    }
-
-    /// Get a transform's dependencies, i.e the outputs wired into the transform's inputs, from its
-    /// TransformIdx.
-    /// The dependencies are ordered by InputIdx. Contains None if argument is currently not
-    /// provided in the graph, Some(Output) otherwise.
-    ///
-    /// Return [`None`] if [`TransformIdx`] does not exist.
-    pub(crate) fn outputs_attached_to_transform(
-        &self,
-        idx: TransformIdx,
-    ) -> Option<Vec<Option<Output>>> {
-        self.get_transform(idx).map(|t| {
-            let len = t.input.len();
-            (0..len)
-                .map(|i| self.output_attached_to(&Input::new(idx, i)))
-                .collect()
-        })
-    }
-
-    fn output_attached_to(&self, input: &Input) -> Option<Output> {
-        for (output, inputs) in self.edges.iter() {
-            if inputs.contains(input) {
-                return Some(*output);
-            }
-        }
-        None
-    }
-
-    pub(crate) fn inputs_attached_to(&self, output: &Output) -> Option<slice::Iter<Input>> {
-        self.edges
-            .get(output)
-            .map(|input_list| input_list.inputs.iter())
-    }
-
     /// Get [`Output`]s of given transformation that are currently in use.
     ///
     /// Return [`None`] if transformation does not exist.
     pub(crate) fn outputs_of_transformation(&self, t_idx: TransformIdx) -> Option<Vec<Output>> {
         self.get_transform(t_idx).map(|t| {
-            let mut outputs = Vec::with_capacity(t.output.len());
-            for i in 0..(t.output.len()) {
+            let len = t.outputs().len();
+            let mut outputs = Vec::with_capacity(len);
+            for i in 0..len {
                 let output = Output::new(t_idx, i);
                 if self.edges.contains_key(&output)
                     || self.outputs.values().any(|&val| Some(output) == val)
@@ -134,59 +33,6 @@ where
             }
             outputs
         })
-    }
-
-    /// Create transform with the [`TransformIdx`] of your choosing.
-    ///
-    /// You need to manage your resource yourself so take care.
-    /// Use [`DST::add_transform`] to have aflak manages resources for you
-    /// (that's probably what your want).
-    pub(crate) fn add_transform_with_idx(
-        &mut self,
-        idx: TransformIdx,
-        t: Bow<'t, Transformation<T, E>>,
-        input_defaults: Vec<Option<T>>,
-    ) {
-        self.transforms
-            .insert(idx, MetaTransform::new_with_defaults(t, input_defaults));
-    }
-
-    /// Remove [`Transformation`] from [`DST`] graph.
-    pub fn remove_transform(
-        &mut self,
-        t_idx: TransformIdx,
-    ) -> Option<TransformAndDefaults<'t, T, E>> {
-        // Remove all connections attached to this transform's outputs
-        if let Some(outputs) = self.outputs_of_transformation(t_idx) {
-            for output in outputs {
-                if let Some(inputs) = self
-                    .inputs_attached_to(&output)
-                    .map(|inputs| inputs.cloned().collect::<Vec<_>>())
-                {
-                    for input in inputs {
-                        self.disconnect(&output, &input);
-                    }
-                }
-                for (_, some_output) in self.outputs.iter_mut() {
-                    if some_output == &Some(output) {
-                        *some_output = None;
-                    }
-                }
-            }
-        }
-
-        // Remove all connections attached to this transform's inputs
-        if let Some(some_outputs) = self.outputs_attached_to_transform(t_idx) {
-            for (i, some_output) in some_outputs.into_iter().enumerate() {
-                if let Some(output) = some_output {
-                    let input = Input::new(t_idx, i);
-                    self.disconnect(&output, &input);
-                }
-            }
-        }
-
-        // Remove transform
-        self.transforms.remove(&t_idx).map(|meta| (meta.tokenize()))
     }
 
     /// Connect an output to an input.
@@ -245,6 +91,69 @@ where
         }
     }
 
+    /// Remove [`Transform`] from [`DST`] graph.
+    pub fn remove_transform(
+        &mut self,
+        t_idx: TransformIdx,
+    ) -> Option<TransformAndDefaults<'t, T, E>> {
+        // Remove all connections attached to this transform's outputs
+        if let Some(outputs) = self.outputs_of_transformation(t_idx) {
+            for output in outputs {
+                if let Some(inputs) = self
+                    .inputs_attached_to(&output)
+                    .map(|inputs| inputs.cloned().collect::<Vec<_>>())
+                {
+                    for input in inputs {
+                        self.disconnect(&output, &input);
+                    }
+                }
+                for (_, some_output) in self.outputs.iter_mut() {
+                    if some_output == &Some(output) {
+                        *some_output = None;
+                    }
+                }
+            }
+        }
+
+        // Remove all connections attached to this transform's inputs
+        if let Some(some_outputs) = self.outputs_attached_to_transform(t_idx) {
+            for (i, some_output) in some_outputs.into_iter().enumerate() {
+                if let Some(output) = some_output {
+                    let input = Input::new(t_idx, i);
+                    self.disconnect(&output, &input);
+                }
+            }
+        }
+
+        // Remove transform
+        self.transforms.remove(&t_idx).map(|meta| (meta.tokenize()))
+    }
+
+    /// Remove node with given ID.
+    pub fn remove_node(&mut self, node_id: &NodeId) {
+        match node_id {
+            NodeId::Output(output_id) => {
+                self.remove_output(output_id);
+            }
+            NodeId::Transform(t_idx) => {
+                self.remove_transform(*t_idx);
+            }
+        }
+    }
+    /// Check if a cycle will be created if the input and output given in argument are connected.
+    ///
+    /// Make dependency list for *output*'s transform and check that it does not depend on
+    /// *input*'s transform.
+    /// Assume input and output exists and that no cycle already exist in the data.
+    fn will_be_cycle(&self, input: &Input, output: &Output) -> bool {
+        for dep in self._dependencies(*output) {
+            if dep.transform_idx() == input.t_idx {
+                return true;
+            }
+        }
+        false
+    }
+
     /// Attach an output to the graph. Only the attached outputs are lazily evaluated.
     /// Return the unique identifier to the attached output.
     /// Return an error if specified output does not exists in current graph.
@@ -259,6 +168,146 @@ where
                 output
             )))
         }
+    }
+
+    /// Check that output exists in the current graph
+    fn output_exists(&self, output: &Output) -> bool {
+        match self.transforms.get(&output.t_idx) {
+            None => false,
+            Some(meta) => meta.transform().output_exists(output.output_i.into()),
+        }
+    }
+
+    /// Check if edge can be added to the current graph.
+    /// Especially check if the input type is the same as the output type.
+    fn is_edge_compatible(&self, input: &Input, output: &Output) -> bool {
+        match (
+            self.get_transform(input.t_idx),
+            self.get_transform(output.t_idx),
+        ) {
+            (Some(input_t), Some(output_t)) => {
+                input_t.nth_input_type(input.input_i.into())
+                    == output_t.nth_output_type(output.output_i.into())
+            }
+            _ => false,
+        }
+    }
+}
+
+impl<'t, T: 't, E: 't> DST<'t, T, E>
+where
+    T: Clone,
+{
+    /// Add a borrowed transform and return its identifier [`TransformIdx`].
+    pub fn add_transform(&mut self, t: &'t Transform<T, E>) -> TransformIdx {
+        self.add_transform_impl(Bow::Borrowed(t))
+    }
+
+    /// Add an owned transform and return its identifier [`TransformIdx`].
+    pub fn add_owned_transform(&mut self, t: Transform<T, E>) -> TransformIdx {
+        self.add_transform_impl(Bow::Owned(t))
+    }
+
+    fn add_transform_impl(&mut self, t: Bow<'t, Transform<T, E>>) -> TransformIdx {
+        let idx = self.new_transform_idx();
+        self.transforms.insert(idx, MetaTransform::new(t));
+        idx
+    }
+
+    /// Make a new empty [`DST`].
+    pub fn new() -> Self {
+        Self {
+            transforms: HashMap::new(),
+            edges: HashMap::new(),
+            outputs: HashMap::new(),
+            cache: HashMap::new(),
+        }
+    }
+
+    /// Get a transform from its [`TransformIdx`].
+    pub fn get_transform(&self, idx: TransformIdx) -> Option<&Transform<T, E>> {
+        self.transforms.get(&idx).map(|t| t.transform())
+    }
+
+    /// Get a transform mutably from its [`TransformIdx`].
+    /// Return `None` if the target transform is not owned.
+    pub fn get_transform_mut(&mut self, idx: TransformIdx) -> Option<&mut Transform<T, E>> {
+        self.transforms
+            .get_mut(&idx)
+            .and_then(|t| t.transform_mut())
+    }
+
+    /// Get a reference to a transform's default inputs from its
+    /// [`TransformIdx`].
+    /// Return [`None`] if the target transform does not exist.
+    pub fn get_default_inputs(&mut self, idx: TransformIdx) -> Option<&[Option<T>]> {
+        self.transforms.get(&idx).map(|t| t.defaults())
+    }
+
+    /// Get a mutable reference to a transform's default inputs from its
+    /// [`TransformIdx`].
+    /// Return [`None`] if the target transform does not exist.
+    pub fn get_default_inputs_mut(&mut self, idx: TransformIdx) -> Option<&mut [Option<T>]> {
+        self.transforms.get_mut(&idx).map(|t| t.defaults_mut())
+    }
+
+    /// Get a node from its [`NodeId`].
+    pub fn get_node(&self, idx: &NodeId) -> Option<Node<T, E>> {
+        match *idx {
+            NodeId::Transform(t_idx) => self.get_transform(t_idx).map(Node::Transform),
+            NodeId::Output(ref output_id) => self
+                .outputs
+                .get(output_id)
+                .map(|some_output| Node::Output(some_output.as_ref())),
+        }
+    }
+
+    /// Get a transform's dependencies, i.e the outputs wired into the transform's inputs, from its
+    /// TransformIdx.
+    /// The dependencies are ordered by InputIdx. Contains None if argument is currently not
+    /// provided in the graph, Some(Output) otherwise.
+    ///
+    /// Return [`None`] if [`TransformIdx`] does not exist.
+    pub(crate) fn outputs_attached_to_transform(
+        &self,
+        idx: TransformIdx,
+    ) -> Option<Vec<Option<Output>>> {
+        self.get_transform(idx).map(|t| {
+            let len = t.inputs().len();
+            (0..len)
+                .map(|i| self.output_attached_to(&Input::new(idx, i)))
+                .collect()
+        })
+    }
+
+    fn output_attached_to(&self, input: &Input) -> Option<Output> {
+        for (output, inputs) in self.edges.iter() {
+            if inputs.contains(input) {
+                return Some(*output);
+            }
+        }
+        None
+    }
+
+    pub(crate) fn inputs_attached_to(&self, output: &Output) -> Option<slice::Iter<Input>> {
+        self.edges
+            .get(output)
+            .map(|input_list| input_list.inputs.iter())
+    }
+
+    /// Create transform with the [`TransformIdx`] of your choosing.
+    ///
+    /// You need to manage your resource yourself so take care.
+    /// Use [`DST::add_transform`] to have aflak manages resources for you
+    /// (that's probably what your want).
+    pub(crate) fn add_transform_with_idx(
+        &mut self,
+        idx: TransformIdx,
+        t: Bow<'t, Transform<T, E>>,
+        input_defaults: Vec<Option<T>>,
+    ) {
+        self.transforms
+            .insert(idx, MetaTransform::new_with_defaults(t, input_defaults));
     }
 
     /// Create a new output not attached and return its Id.
@@ -304,31 +353,11 @@ where
         self.outputs.remove(output_id);
     }
 
-    /// Remove node with given ID.
-    pub fn remove_node(&mut self, node_id: &NodeId) {
-        match node_id {
-            NodeId::Output(output_id) => {
-                self.remove_output(output_id);
-            }
-            NodeId::Transform(t_idx) => {
-                self.remove_transform(*t_idx);
-            }
-        }
-    }
-
     /// Check that input exists in the current graph
     fn input_exists(&self, input: &Input) -> bool {
         match self.transforms.get(&input.t_idx) {
             None => false,
             Some(meta) => meta.transform().input_exists(input.input_i.into()),
-        }
-    }
-
-    /// Check that output exists in the current graph
-    fn output_exists(&self, output: &Output) -> bool {
-        match self.transforms.get(&output.t_idx) {
-            None => false,
-            Some(meta) => meta.transform().output_exists(output.output_i.into()),
         }
     }
 
@@ -338,35 +367,6 @@ where
             .get(&output)
             .map(|input_list| input_list.contains(input))
             .unwrap_or(false)
-    }
-
-    /// Check if a cycle will be created if the input and output given in argument are connected.
-    ///
-    /// Make dependency list for *output*'s transform and check that it does not depend on
-    /// *input*'s transform.
-    /// Assume input and output exists and that no cycle already exist in the data.
-    fn will_be_cycle(&self, input: &Input, output: &Output) -> bool {
-        for dep in self._dependencies(*output) {
-            if dep.transform_idx() == input.t_idx {
-                return true;
-            }
-        }
-        false
-    }
-
-    /// Check if edge can be added to the current graph.
-    /// Especially check if the input type is the same as the output type.
-    fn is_edge_compatible(&self, input: &Input, output: &Output) -> bool {
-        match (
-            self.get_transform(input.t_idx),
-            self.get_transform(output.t_idx),
-        ) {
-            (Some(input_t), Some(output_t)) => {
-                input_t.nth_input_type(input.input_i.into())
-                    == output_t.nth_output_type(output.output_i.into())
-            }
-            _ => false,
-        }
     }
 
     fn new_transform_idx(&self) -> TransformIdx {

src/aflak_cake/src/dst/compute.rs
@@ -28,7 +28,6 @@ where
             .ok_or_else(|| {
                 DSTError::ComputeError(format!("Transform {:?} not found!", output.t_idx))
             })?;
-        let mut op = t.start();
         let mut results = Vec::with_capacity(deps.len());
         for _ in 0..(deps.len()) {
             results.push(Err(DSTError::NothingDoneYet));
@@ -49,9 +48,17 @@ where
                 })
             }
         });
+
+        let mut oks = Vec::with_capacity(results.len());
         for result in results {
-            op.feed(result?);
+            oks.push(result?);
         }
+
+        let mut op = t.start();
+        for r in &oks {
+            op.feed(r);
+        }
+
         match op.call().nth(output.output_i.into()) {
             None => Err(DSTError::ComputeError(
                 "No nth output received. This is a bug!".to_owned(),
@@ -84,7 +91,7 @@ where
 
 impl<'t, T, E> DST<'t, T, E>
 where
-    T: 't + Clone,
+    T: 't + Clone + VariantName,
     E: 't,
 {
     /// Purge all cache in the given output and all its children.

commit 115e5b70ebad67d7cc5a2e4ef241eb7722bcbd30

[node_editor] Fix regression on updating constant node
Regression was caused by a75826a14524e2e9b21a5e676f1c40d148650c63.
During refactoring, a "!=" operator mysteriously turned into a "=="
operator, causing a wrong behaviour in which a constant node could not
be updated from an output window.

src/node_editor/src/editor.rs
@@ -110,7 +110,7 @@ where
         if let Some(t) = self.dst.get_transform_mut(id) {
             let mut new_value = false;
             if let cake::Algorithm::Constant(ref constant) = t.algorithm() {
-                new_value = *constant == val;
+                new_value = *constant != val;
             }
             if new_value {
                 t.set_constant(val);