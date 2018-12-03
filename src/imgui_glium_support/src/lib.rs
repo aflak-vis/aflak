09fed07e116dfeea6160f016f13cd1e76ba090cb
Malik Olivier Boussejra <malik@boussejra.com>
2018-12-03 17:46:17 +0900

    [imgui_glium_support] Update against latest support in imgui
    Basically put the content of the imgui-glutin-support crate [0] into
    glutin_support.rs.
    We now have more control on DPI.
    
    [0] https://github.com/Gekkio/imgui-rs/blob/0440752/imgui-glutin-support/src/lib.rs

--- src/imgui_glium_support/src/lib.rs
+++ src/imgui_glium_support/src/lib.rs
@@ -3,6 +3,8 @@ extern crate glium;
 extern crate imgui;
 extern crate imgui_glium_renderer;
 
+mod glutin_support;
+
 use std::rc::Rc;
 use std::result;
 use std::time::Instant;
@@ -11,7 +13,7 @@ use glium::{
     backend::{glutin::DisplayCreationError, Context, Facade},
     glutin, Display, Surface, SwapBuffersError, Texture2d,
 };
-use imgui::{FrameSize, ImGui, ImGuiMouseCursor, ImString, Textures, Ui};
+use imgui::{ImGui, ImString, Textures, Ui};
 use imgui_glium_renderer::{Renderer, RendererError};
 
 #[derive(Copy, Clone, PartialEq, Debug, Default)]
@@ -90,74 +92,31 @@ where
     imgui.set_ini_filename(config.ini_filename);
     imgui.set_log_filename(config.log_filename);
 
+    // We only use integer DPI factors, because the UI can get very blurry
+    // otherwise.
+    let hidpi_factor = window.get_hidpi_factor().round();
+
     let mut renderer = Renderer::init(&mut imgui, &display).expect("Failed to initialize renderer");
 
-    configure_keys(&mut imgui);
+    glutin_support::configure_keys(&mut imgui);
 
     let mut last_frame = Instant::now();
-    let mut mouse_state = MouseState::default();
     let mut quit = false;
 
     loop {
         events_loop.poll_events(|event| {
-            use glium::glutin::ElementState::Pressed;
-            use glium::glutin::WindowEvent::*;
-            use glium::glutin::{Event, MouseButton, MouseScrollDelta, TouchPhase};
+            use glium::glutin::{Event, WindowEvent::CloseRequested};
+
+            glutin_support::handle_event(
+                &mut imgui,
+                &event,
+                window.get_hidpi_factor(),
+                hidpi_factor,
+            );
 
             if let Event::WindowEvent { event, .. } = event {
                 match event {
                     CloseRequested => quit = true,
-                    KeyboardInput { input, .. } => {
-                        use glium::glutin::VirtualKeyCode as Key;
-
-                        let pressed = input.state == Pressed;
-                        match input.virtual_keycode {
-                            Some(Key::Tab) => imgui.set_key(0, pressed),
-                            Some(Key::Left) => imgui.set_key(1, pressed),
-                            Some(Key::Right) => imgui.set_key(2, pressed),
-                            Some(Key::Up) => imgui.set_key(3, pressed),
-                            Some(Key::Down) => imgui.set_key(4, pressed),
-                            Some(Key::PageUp) => imgui.set_key(5, pressed),
-                            Some(Key::PageDown) => imgui.set_key(6, pressed),
-                            Some(Key::Home) => imgui.set_key(7, pressed),
-                            Some(Key::End) => imgui.set_key(8, pressed),
-                            Some(Key::Delete) => imgui.set_key(9, pressed),
-                            Some(Key::Back) => imgui.set_key(10, pressed),
-                            Some(Key::Return) => imgui.set_key(11, pressed),
-                            Some(Key::Escape) => imgui.set_key(12, pressed),
-                            Some(Key::A) => imgui.set_key(13, pressed),
-                            Some(Key::C) => imgui.set_key(14, pressed),
-                            Some(Key::V) => imgui.set_key(15, pressed),
-                            Some(Key::X) => imgui.set_key(16, pressed),
-                            Some(Key::Y) => imgui.set_key(17, pressed),
-                            Some(Key::Z) => imgui.set_key(18, pressed),
-                            Some(Key::LControl) | Some(Key::RControl) => {
-                                imgui.set_key_ctrl(pressed)
-                            }
-                            Some(Key::LShift) | Some(Key::RShift) => imgui.set_key_shift(pressed),
-                            Some(Key::LAlt) | Some(Key::RAlt) => imgui.set_key_alt(pressed),
-                            Some(Key::LWin) | Some(Key::RWin) => imgui.set_key_super(pressed),
-                            _ => {}
-                        }
-                    }
-                    CursorMoved { position: pos, .. } => mouse_state.pos = pos.into(),
-                    MouseInput { state, button, .. } => match button {
-                        MouseButton::Left => mouse_state.pressed.0 = state == Pressed,
-                        MouseButton::Right => mouse_state.pressed.1 = state == Pressed,
-                        MouseButton::Middle => mouse_state.pressed.2 = state == Pressed,
-                        _ => {}
-                    },
-                    MouseWheel {
-                        delta: MouseScrollDelta::LineDelta(_, y),
-                        phase: TouchPhase::Moved,
-                        ..
-                    } => mouse_state.wheel = y,
-                    MouseWheel {
-                        delta: MouseScrollDelta::PixelDelta(pos),
-                        phase: TouchPhase::Moved,
-                        ..
-                    } => mouse_state.wheel = pos.y as f32,
-                    ReceivedCharacter(c) => imgui.add_input_character(c),
                     _ => (),
                 }
             }
@@ -168,36 +127,9 @@ where
         let delta_s = delta.as_secs() as f32 + delta.subsec_nanos() as f32 / 1_000_000_000.0;
         last_frame = now;
 
-        update_mouse(&mut imgui, &mut mouse_state);
+        glutin_support::update_mouse_cursor(&imgui, &window);
 
-        let mouse_cursor = imgui.mouse_cursor();
-        if imgui.mouse_draw_cursor() || mouse_cursor == ImGuiMouseCursor::None {
-            // Hide OS cursor
-            window.hide_cursor(true);
-        } else {
-            // Set OS cursor
-            window.hide_cursor(false);
-            window.set_cursor(match mouse_cursor {
-                ImGuiMouseCursor::None => unreachable!("mouse_cursor was None!"),
-                ImGuiMouseCursor::Arrow => glutin::MouseCursor::Arrow,
-                ImGuiMouseCursor::TextInput => glutin::MouseCursor::Text,
-                ImGuiMouseCursor::ResizeAll => glutin::MouseCursor::Move,
-                ImGuiMouseCursor::ResizeNS => glutin::MouseCursor::NsResize,
-                ImGuiMouseCursor::ResizeEW => glutin::MouseCursor::EwResize,
-                ImGuiMouseCursor::ResizeNESW => glutin::MouseCursor::NeswResize,
-                ImGuiMouseCursor::ResizeNWSE => glutin::MouseCursor::NwseResize,
-                ImGuiMouseCursor::Hand => glutin::MouseCursor::Hand,
-            });
-        }
-
-        let logical_size = window
-            .get_inner_size()
-            .ok_or_else(|| Error::Message("Window no longer exists!".to_owned()))?;
-
-        let frame_size = FrameSize {
-            logical_size: logical_size.into(),
-            hidpi_factor: window.get_hidpi_factor(),
-        };
+        let frame_size = glutin_support::get_frame_size(&window, hidpi_factor).unwrap();
 
         let ui = imgui.frame(frame_size, delta_s);
         if !run_ui(&ui, display.get_context(), renderer.textures()) {
@@ -220,41 +152,4 @@ where
     }
 
     Ok(())
-}
-
-fn configure_keys(imgui: &mut ImGui) {
-    use imgui::ImGuiKey;
-
-    imgui.set_imgui_key(ImGuiKey::Tab, 0);
-    imgui.set_imgui_key(ImGuiKey::LeftArrow, 1);
-    imgui.set_imgui_key(ImGuiKey::RightArrow, 2);
-    imgui.set_imgui_key(ImGuiKey::UpArrow, 3);
-    imgui.set_imgui_key(ImGuiKey::DownArrow, 4);
-    imgui.set_imgui_key(ImGuiKey::PageUp, 5);
-    imgui.set_imgui_key(ImGuiKey::PageDown, 6);
-    imgui.set_imgui_key(ImGuiKey::Home, 7);
-    imgui.set_imgui_key(ImGuiKey::End, 8);
-    imgui.set_imgui_key(ImGuiKey::Delete, 9);
-    imgui.set_imgui_key(ImGuiKey::Backspace, 10);
-    imgui.set_imgui_key(ImGuiKey::Enter, 11);
-    imgui.set_imgui_key(ImGuiKey::Escape, 12);
-    imgui.set_imgui_key(ImGuiKey::A, 13);
-    imgui.set_imgui_key(ImGuiKey::C, 14);
-    imgui.set_imgui_key(ImGuiKey::V, 15);
-    imgui.set_imgui_key(ImGuiKey::X, 16);
-    imgui.set_imgui_key(ImGuiKey::Y, 17);
-    imgui.set_imgui_key(ImGuiKey::Z, 18);
-}
-
-fn update_mouse(imgui: &mut ImGui, mouse_state: &mut MouseState) {
-    imgui.set_mouse_pos(mouse_state.pos.0 as f32, mouse_state.pos.1 as f32);
-    imgui.set_mouse_down([
-        mouse_state.pressed.0,
-        mouse_state.pressed.1,
-        mouse_state.pressed.2,
-        false,
-        false,
-    ]);
-    imgui.set_mouse_wheel(mouse_state.wheel);
-    mouse_state.wheel = 0.0;
 }
