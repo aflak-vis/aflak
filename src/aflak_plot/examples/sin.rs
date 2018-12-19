e008fd966e7e8dac984b84a95571f91cd15b1e65
Malik Olivier Boussejra <malik@boussejra.com>
2018-12-19 21:41:41 +0900

    Extensive example

--- src/aflak_plot/examples/sin.rs
+++ src/aflak_plot/examples/sin.rs
@@ -5,6 +5,7 @@ extern crate aflak_plot;
 extern crate ndarray;
 
 use aflak_plot::{
+    imshow::{self, UiImage2d},
     plot::{self, UiImage1d},
     AxisTransform,
 };
@@ -18,11 +19,13 @@ fn main() -> Result<(), support::Error> {
         ..Default::default()
     };
     let mut state = plot::State::default();
+    let mut state2 = imshow::State::default();
+    let texture_id = imgui::ImTexture::from(1);
 
     const MAX: f32 = 4.0 * f32::consts::PI;
     let sin = ndarray::Array1::linspace(0.0, MAX, 100).mapv_into(f32::sin);
 
-    support::run(config, |ui, _, _| {
+    support::run(config, |ui, gl_ctx, textures| {
         ui.window(im_str!("Sin")).build(|| {
             ui.image1d(
                 &sin,
@@ -31,6 +34,43 @@ fn main() -> Result<(), support::Error> {
                 &mut state,
             )
             .expect("Image1d failed");
+        });
+        if state2.image_created_on().is_none() {
+            const WIDTH: usize = 200;
+            const HEIGHT: usize = 100;
+            let image_data = ndarray::Array2::from_shape_fn([HEIGHT, WIDTH], |(j, i)| {
+                use std::f32;
+                let i = i as isize;
+                let j = j as isize;
+                let width = WIDTH as isize;
+                let height = HEIGHT as isize;
+                let sin = f32::sin((i - width / 3) as f32 / WIDTH as f32 * 2.0 * f32::consts::PI);
+                let cos = f32::cos((j - height / 3) as f32 / HEIGHT as f32 * 2.0 * f32::consts::PI);
+                f32::exp(sin * sin + cos * cos)
+            })
+            .into_dimensionality()
+            .unwrap();
+            state2
+                .set_image(
+                    image_data,
+                    ::std::time::Instant::now(),
+                    gl_ctx,
+                    texture_id,
+                    textures,
+                )
+                .unwrap();
+        }
+        ui.window(im_str!("Peak")).build(|| {
+            ui.image2d(
+                gl_ctx,
+                textures,
+                texture_id,
+                "exp(sin(x)^2 + cos(y)^2)",
+                Some(&AxisTransform::new("X Axis", |x| x)),
+                Some(&AxisTransform::new("Y Axis", |y| y)),
+                &mut state2,
+            )
+            .expect("Image2d failed");
         });
         true
     })
