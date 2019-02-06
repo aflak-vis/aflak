0ef91a278938b6ce4180334c0f6e5546d0997c25
Malik Olivier Boussejra <malik@boussejra.com>
2019-02-07 01:03:03 +0900

    [node_editor] constant_editor: Add documentation

--- src/node_editor/src/constant_editor.rs
+++ src/node_editor/src/constant_editor.rs
@@ -1,5 +1,10 @@
 use imgui::{ImId, Ui};
 
+/// Trait to define how to draw constant editor.
+///
+/// Constant values may be edited in the node editor.
+/// Implementing this trait is necessary do define how the values flowing around
+/// in the node editor can be manually edited.
 pub trait ConstantEditor<T>: Default {
     /// Build editor for constant T and return new value if value changed
     fn editor<'a, I>(&self, ui: &Ui, constant: &T, id: I, read_only: bool) -> Option<T>
