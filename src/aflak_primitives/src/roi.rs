87699233d06c74618a20a020410e83a5b75af410
Malik Olivier Boussejra <malik@boussejra.com>
2019-02-07 00:09:57 +0900

    [primitives] roi: Add documentation

--- src/aflak_primitives/src/roi.rs
+++ src/aflak_primitives/src/roi.rs
@@ -1,12 +1,17 @@
 use ndarray::ArrayView2;
 
+/// A region of interest in a 2D image.
 #[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
 pub enum ROI {
+    /// The whole image is a region of interest.
     All,
+    /// The list of pixels selected by this region of interest.
     PixelList(Vec<(usize, usize)>),
 }
 
 impl ROI {
+    /// Get the value of each point of the 2D image in the region of interest,
+    /// along with the original coordinate of each selected pixel.
     pub fn filter(&self, data: ArrayView2<f32>) -> Vec<((usize, usize), f32)> {
         match *self {
             ROI::All => {
