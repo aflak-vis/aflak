b9ac2d7718c6754e7d8cf9c990ab4e06fe241e77
Malik Olivier Boussejra <malik@boussejra.com>
2019-03-19 05:55:45 +0900

    [node_editor] event: Implement Debug for RenderEvent

--- src/node_editor/src/event.rs
+++ src/node_editor/src/event.rs
@@ -1,3 +1,5 @@
+use std::fmt;
+
 use cake::{InputSlot, NodeId, Output, Transform, TransformIdx};
 
 pub enum RenderEvent<T: 'static, E: 'static> {
@@ -15,4 +17,28 @@ pub enum RenderEvent<T: 'static, E: 'static> {
     Import,
     Export,
     AddNewMacro,
+}
+
+impl<T, E> fmt::Debug for RenderEvent<T, E> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        use self::RenderEvent::*;
+        match self {
+            Connect(o, i) => write!(f, "Connect({:?}, {:?})", o, i),
+            AddTransform(_) => write!(f, "AddTransform(_)"),
+            CreateOutput => write!(f, "CreateOutput"),
+            AddConstant(name) => write!(f, "AddConstant({:?})", name),
+            SetConstant(t_idx, _) => write!(f, "SetConstant({:?}, _)", t_idx),
+            WriteDefaultInput {
+                t_idx, input_index, ..
+            } => write!(
+                f,
+                "WriteDefaultInput {{ t_idx: {:?}, input_index: {:?}, .. }}",
+                t_idx, input_index
+            ),
+            RemoveNode(node_id) => write!(f, "RemoveNode({:?})", node_id),
+            Import => write!(f, "Import"),
+            Export => write!(f, "Export"),
+            AddNewMacro => write!(f, "AddNewMacro"),
+        }
+    }
 }
