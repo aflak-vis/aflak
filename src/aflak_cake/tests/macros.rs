018ef76ff16e2ca5f7e2c9658c97055ba28d598e
Malik Olivier Boussejra <malik@boussejra.com>
2019-03-20 17:56:55 +0900

    [cake] tests/macros: Improve tree sketch
    The two "parts" of the tree are completely unconnected, so move them
    farther away from one another to make the sketch more readable.

--- src/aflak_cake/tests/macros.rs
+++ src/aflak_cake/tests/macros.rs
@@ -20,9 +20,9 @@ fn make_macro() -> aflak_cake::macros::MacroHandle<'static, AlgoIO, E> {
     if let &[plus1, minus1, _, _, _] = *TRANSFORMATIONS_REF {
         // An arrow points from a box's input to a box's output  `OUT -> INT`
         // We build the dst as follows (all functions are trivial and only have 1 output or 0/1 input):
-        //    0 (default input) ---\
-        //EMPTY                     c, plus1 -> d, plus1 -> OUT1
-        // \-> b, minus1 -> OUT2     \-> e, plus1
+        //           0 (default input) ---\
+        //EMPTY                            c, plus1 -> d, plus1 -> OUT1
+        // \-> b, minus1 -> OUT2            \-> e, plus1
         let mut dst = DST::new();
         let b = dst.add_transform(&minus1);
         let c = dst.add_transform(&plus1);
