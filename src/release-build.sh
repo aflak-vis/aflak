db1c73bcf9e0f34ac35e03c7d42008f30b8fc94d
Malik Olivier Boussejra <malik@boussejra.com>
2019-03-24 19:06:47 +0800

    Add script to make releases

---
+++ src/release-build.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+set -eux
+
+REV=$(git rev-parse --short=9 HEAD)
+ARCHIVE_NAME="aflak-$REV.tar.gz"
+OS=linux64
+
+cargo clean
+echo '[profile.release]' >> Cargo.toml
+echo 'lto = true' >> Cargo.toml
+cargo build --release
+strip target/release/aflak
+tar cvf - -C target/release aflak | gzip --best > "$ARCHIVE_NAME"
+
+rm -rf aflak-vis
+git clone git@github.com:aflak-vis/aflak-vis.github.io.git aflak-vis
+mkdir -p "aflak-vis/download/build/$OS"
+mv "$ARCHIVE_NAME" "aflak-vis/download/build/$OS"
+cd aflak-vis
+git add "download/build/$OS/$ARCHIVE_NAME"
+git commit -m "Release $REV for $OS"
+git push
