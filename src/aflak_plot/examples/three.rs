297ad3da188e7c1ae56d992608c2b6c52aade0b0
MsrOhkwr <m.ohkawara@keio.jp>
2019-08-30 20:10:41 +0900

    introduce GPU raycasting

--- src/aflak_plot/examples/three.rs
+++ src/aflak_plot/examples/three.rs
@@ -15,7 +15,19 @@ fn main() {
     };
     let texture_id = imgui::ImTexture::from(1);
 
-    let image = ndarray::Array3::<f32>::zeros((10, 10, 10));
+    let d = 32 as isize;
+    let mut image = ndarray::Array3::<f32>::zeros((d as usize, d as usize, d as usize));
+    for i in 0..d {
+        for j in 0..d {
+            for k in 0..d {
+                image[[i as usize, j as usize, k as usize]] = 8f32
+                    - ((i as f32 - d as f32 / 2f32 + 0.5f32).powi(2)
+                        + (j as f32 - d as f32 / 2f32 + 0.5f32).powi(2)
+                        + (k as f32 - d as f32 / 2f32 + 0.5f32).powi(2))
+                        / (d as f32 / 2f32).powi(2);
+            }
+        }
+    }
     let image = image.into_dyn();
 
     support::run(config, |ui, gl_ctx, textures| {
