848b9b7ee9f1e256e5bcb2f1da12938b0b350d16
Rikuo Uchiki <dbkn_rikuo@keio.jp>
2019-10-19 14:18:38 +0900

    [plot] Add Line structure for arbitrary slicing

--- src/aflak_plot/src/plot/state.rs
+++ src/aflak_plot/src/plot/state.rs
@@ -211,6 +211,7 @@ impl State {
                 // Unused in plot
                 Interaction::HorizontalLine(_) => {}
                 Interaction::FinedGrainedROI(_) => {}
+                Interaction::Line(_) => {}
             }
             stack.pop(ui);
         }
