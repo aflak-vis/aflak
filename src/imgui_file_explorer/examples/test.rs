752c7da481f10c5c554f02545b4f5bec11bce88e
Malik Olivier Boussejra <malik@boussejra.com>
2019-10-28 07:46:04 +0900

    [file_explorer] Compile example with new imgui API

--- src/imgui_file_explorer/examples/test.rs
+++ src/imgui_file_explorer/examples/test.rs
@@ -2,19 +2,17 @@ extern crate glium;
 extern crate imgui;
 extern crate imgui_file_explorer;
 extern crate imgui_glium_renderer;
+extern crate imgui_winit_support;
 use imgui::*;
 use imgui_file_explorer::UiFileExplorer;
 
 mod support;
 
-const CLEAR_COLOR: [f32; 4] = [1.0, 1.0, 1.0, 1.0];
-
 fn test(ui: &Ui) {
-    let window = ui
-        .window(im_str!("File Explorer"))
-        .size((600.0, 400.0), ImGuiCond::Appearing)
-        .position((200.0, 200.0), ImGuiCond::FirstUseEver);
-    window.build(|| {
+    let window = Window::new(im_str!("File Explorer"))
+        .size([600.0, 400.0], Condition::Appearing)
+        .position([200.0, 200.0], Condition::FirstUseEver);
+    window.build(ui, || {
         ui.push_item_width(-140.0);
         let file = ui.file_explorer("/", &["fits", "csv"]);
         if let Ok(Some(file)) = file {
@@ -24,8 +22,7 @@ fn test(ui: &Ui) {
 }
 
 fn main() {
-    support::run("imgui-file-explorer-test".to_owned(), CLEAR_COLOR, |ui| {
+    support::init("imgui-file-explorer-test").main_loop(|_run, ui| {
         test(ui);
-        true
     });
 }
