f8db8fcbbb1d545207c7ae7fe2c58d77ae537b5f
Rikuo Uchiki <dbkn_rikuo@keio.jp>
2019-10-29 13:30:52 +0900

    [primitives] Add temporary filter

--- src/aflak_primitives/src/roi.rs
+++ src/aflak_primitives/src/roi.rs
@@ -36,6 +36,30 @@ impl ROI {
         }
     }
 
+    pub fn filterx(&self, data: ArrayView2<f32>) -> Vec<((usize, usize), f32)> {
+        match *self {
+            ROI::All => {
+                let mut out = Vec::with_capacity(data.len());
+                let size = data.dim();
+                for j in 0..size.1 {
+                    for i in 0..size.0 {
+                        out.push(((i, j), *data.get([i, j]).unwrap()));
+                    }
+                }
+                out
+            }
+            ROI::PixelList(ref pixels) => {
+                let mut out = Vec::with_capacity(pixels.len());
+                for &(i, j) in pixels {
+                    if let Some(val) = data.get([73 - j, i]) {
+                        out.push(((i, j), *val));
+                    }
+                }
+                out
+            }
+        }
+    }
+
     pub fn datalen(&self) -> usize {
         match *self {
             ROI::All => 0,
