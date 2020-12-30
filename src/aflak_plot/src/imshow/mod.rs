dbd0154b284f395493961d0401ed5d3f4dd900c0
Rikuo Uchiki <dbkn_rikuo@keio.jp>
2020-12-30 21:33:32 +0900

    [plot] Add color_image for image2d

--- src/aflak_plot/src/imshow/mod.rs
+++ src/aflak_plot/src/imshow/mod.rs
@@ -122,6 +122,55 @@ impl<'ui> UiImage2d for Ui<'ui> {
 
         Ok(())
     }
+
+    fn color_image<F, FX, FY, I>(
+        &self,
+        ctx: &F,
+        textures: &mut Textures,
+        texture_id: TextureId,
+        vunit: &str,
+        xaxis: Option<&AxisTransform<FX>>,
+        yaxis: Option<&AxisTransform<FY>>,
+        state: &mut State<I>,
+    ) -> Result<(), Error>
+    where
+        F: Facade,
+        FX: Fn(f32) -> f32,
+        FY: Fn(f32) -> f32,
+        I: Borrow<ArrayD<f32>>,
+    {
+        let window_pos = self.window_pos();
+        let cursor_pos = self.cursor_screen_pos();
+        let window_size = self.window_size();
+        const HIST_WIDTH: f32 = 40.0;
+        const BAR_WIDTH: f32 = 20.0;
+
+        const RIGHT_PADDING: f32 = 100.0;
+        let image_max_size = (
+            // Add right padding so that ticks and labels on the right fits
+            window_size[0] - HIST_WIDTH - BAR_WIDTH - RIGHT_PADDING,
+            window_size[1] - (cursor_pos[1] - window_pos[1]),
+        );
+        let ([p, size], x_label_height) =
+            state.show_image(self, texture_id, vunit, xaxis, yaxis, image_max_size)?;
+
+        /*state.show_hist(self, [p[0] + size[0], p[1]], [HIST_WIDTH, size[1]]);
+        //let lut_bar_updated = state.show_bar(
+            self,
+            [p[0] + size[0] + HIST_WIDTH, p[1]],
+            [BAR_WIDTH, size[1]],
+        );
+        if lut_bar_updated {
+            state
+                .image()
+                .update_texture(ctx, texture_id, textures, &state.lut)?;
+        }
+
+        self.set_cursor_screen_pos([p[0], p[1] + size[1] + x_label_height]);
+        state.show_roi_selector(self);*/
+
+        Ok(())
+    }
 }
 
 /// Implementation of a UI to visualize a 2D image with ImGui and OpenGL.
@@ -141,4 +190,20 @@ pub trait UiImage2d {
         FX: Fn(f32) -> f32,
         FY: Fn(f32) -> f32,
         I: Borrow<ArrayD<f32>>;
+
+    fn color_image<F, FX, FY, I>(
+            &self,
+            ctx: &F,
+            textures: &mut Textures,
+            texture_id: TextureId,
+            vunit: &str,
+            xaxis: Option<&AxisTransform<FX>>,
+            yaxis: Option<&AxisTransform<FY>>,
+            state: &mut State<I>,
+        ) -> Result<(), Error>
+        where
+            F: Facade,
+            FX: Fn(f32) -> f32,
+            FY: Fn(f32) -> f32,
+            I: Borrow<ArrayD<f32>>;
 }
