cf4a984f6ee13825a35a390f29ce5a8a5f487c6b
Rikuo Uchiki <dbkn_rikuo@keio.jp>
2021-02-17 12:14:28 +0900

    [cake] Add MacroID to TransformIdx

--- src/aflak_cake/src/dst/build.rs
+++ src/aflak_cake/src/dst/build.rs
@@ -12,6 +12,7 @@ use crate::dst::{
 };
 use crate::dst::{MetaTransform, TransformAndDefaults};
 use crate::transform::Transform;
+use uuid::Uuid;
 
 impl<'t, T: 't, E: 't> DST<'t, T, E>
 where
@@ -402,7 +403,7 @@ impl<'t, T: 't, E: 't> DST<'t, T, E> {
         self.transforms
             .keys()
             .max()
-            .unwrap_or(&TransformIdx(0))
+            .unwrap_or(&TransformIdx(None, 0))
             .incr()
     }
 
